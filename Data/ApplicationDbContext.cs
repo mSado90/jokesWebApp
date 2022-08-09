352df72e6583451ac5f15c62bbecbed657236616
Michał Sadecki <michalsadecki90@gmail.com>
2022-08-09 16:45:52 +0200

    initial commit, database created and running

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using jokesWebApp.Models;
+
+namespace jokesWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<jokesWebApp.Models.Joke> Joke { get; set; }
+    }
+}
---
+++ Models/Joke.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public String JokeQuestion { get; set; }
+        public String JokeAnswer { get; set; }
+
+        public Joke()
+        {
+               
+        }
+    }
+}
